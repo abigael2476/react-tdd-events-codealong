840ab3dda20186308804b732cfd7ebca442e9413
abigaelseenoi <abigaelseenoi5@gmail.com>
2025-10-21 13:53:37 +0300

    added files

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,50 @@
 import { render, screen } from "@testing-library/react";
-import App from "../App";
-
+import userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom';
 
-// Code tests here
+import App from "../App";
+
+// Pepperoni checkbox
+test("checkbox is initially unchecked", () => {
+  render(<App />);
+
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+  expect(addPepperoni).not.toBeChecked();
+});
+
+test("checkbox appears as checked when user clicks it", () => {
+  render(<App />);
+
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+  userEvent.click(addPepperoni);
+  expect(addPepperoni).toBeChecked();
+});
+
+test("checkbox appears as unchecked when user clicks a second time", () => {
+  render(<App />);
+
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
+  userEvent.click(addPepperoni);
+
+  expect(addPepperoni).toBeChecked();
+
+  userEvent.click(addPepperoni);
+
+  expect(addPepperoni).not.toBeChecked();
+});
+
+// Size select element
+test("size select element initially displays 'Small'", () => {
+  render(<App />);
+  const selectSize = screen.getByLabelText(/select size/i);
+  expect(selectSize).toHaveDisplayValue("Small");
+});
+
+// "Your Selection" text
+
+// "Contact Info" text box
+
+// Submit Order button
